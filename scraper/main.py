51f2caf4f26b9e2319ad7e227ba1c7a66a614c70
dmun <davidru01@live.nl>
2025-01-06 00:36:59 +0100

    progperss

--- scraper/main.py
+++ scraper/main.py
@@ -3,108 +3,262 @@
 %autoreload 2
 """
 
-from pprint import pprint
-import numpy as np
+import time
+from nltk.corpus import stopwords
 import gensim
 import gensim.corpora as corpora
-import matplotlib.pyplot as plt
-import nltk
 import spacy
 import polars as pl
+from spacy.tokens import Doc
 import util
-from gensim.models import CoherenceModel, LdaModel
-from gensim.utils import simple_preprocess
-from IPython.display import display
+from gensim.models import LdaModel
 import pyLDAvis
 import pyLDAvis.gensim
+from tqdm import tqdm
+import random
+from pprint import pprint
 
 # %%
 path = "data/ADHD_submissions.zst"
 keywords = ["hack", "tip", "advice"]
 columns = [
-    "id",
-    "title",
-    "selftext",
-    "created_utc",
-    "num_comments",
-    "score",
-    # "link_flair_type",
-    # "gilded",
+  "id",
+  "title",
+  "selftext",
+  "created_utc",
+  "num_comments",
+  "score",
+  # "link_flair_type",
+  # "gilded",
 ]
 
-# %%
-with open(path, "rb") as f:
-    stream = util.get_zst_file_json_stream(f)
-    submission = next(stream)
-    submission["created_utc"]
 
 # %%
-with open(path, "rb") as f:
+def get_submissions(path: str):
+  with open(path, "rb") as f:
     print(f"Processing file {path}")
 
-    records = []
-    for row in util.get_zst_file_json_stream(f):
-        row["title"] = row["title"].lower()
-        row["selftext"] = row["selftext"].lower()
+    row_it = util.get_zst_file_json_stream(f)
+    schema = {
+      "archived": pl.Boolean,
+      "id": pl.Utf8,
+      "url": pl.Utf8,
+      "title": pl.Utf8,
+      "author": pl.Utf8,
+      "selftext": pl.Utf8,
+      "score": pl.Int32,
+      "created_utc": pl.Int64,
+      "hidden": pl.Boolean,
+      "link_flair_richtext": pl.List(pl.Utf8),
+      # "link_flair_text": pl.Null,
+      "link_flair_type": pl.Utf8,
+      "locked": pl.Boolean,
+      "num_comments": pl.Int32,
+      "num_crossposts": pl.Int32,
+      "over_18": pl.Boolean,
+      "retrieved_on": pl.Int64,
+      "send_replies": pl.Boolean,
+      "spoiler": pl.Boolean,
+      "stickied": pl.Boolean,
+    }
+    df = pl.LazyFrame(row_it, schema=schema, strict=False, infer_schema_length=1000)
 
-        if any([word in row["title"] or word in row["selftext"] for word in keywords]):
-            records.append({col: row[col] for col in columns})
+    df = df.filter(
+      (pl.col("title").str.contains(r"\?"))
+      & (pl.col("selftext").str.contains("|".join(keywords)))
+    ).select(columns)
 
-    data = pl.DataFrame(records)
+    # print(f"collected {len(df.collect())} submissions")
+    return df
 
-    print(f"collected {len(data)} submissions")
+
+df_submissions = get_submissions("data/ADHD_submissions.zst")
 
 # %%
-df = data.unique("title")
-df = df.filter(~pl.col("selftext").str.contains(r"^\[(removed)|(deleted)\]$"))
-df = df.head(100)
+ids = df_submissions.select("id").collect()["id"].to_list()
+
+# %%
+df_submissions.select("title").collect().sample()["title"][0]
 
 
 # %%
-def lemmatize(text: str, allowed_postags=["NOUN", "ADJ", "VERB", "ADV"]):
-    nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
-    return " ".join([x.lemma_ for x in nlp(text) if x.pos_ in allowed_postags])
+def get_comments(path: str):
+  with open(path, "rb") as f:
+    print(f"Processing file {path}")
 
+    row_it = util.get_zst_file_json_stream(f)
+    schema = {
+      "archived": pl.Boolean,
+      "link_id": pl.Utf8,
+      "parent_id": pl.Utf8,
+      "id": pl.Utf8,
+      "url": pl.Utf8,
+      "title": pl.Utf8,
+      "author": pl.Utf8,
+      "body": pl.Utf8,
+      "score": pl.Int32,
+      "created_utc": pl.Int64,
+      "hidden": pl.Boolean,
+      "link_flair_richtext": pl.List(pl.Utf8),
+      "link_flair_type": pl.Utf8,
+      "locked": pl.Boolean,
+      "num_comments": pl.Int32,
+      "num_crossposts": pl.Int32,
+      "over_18": pl.Boolean,
+      "retrieved_on": pl.Int64,
+      "send_replies": pl.Boolean,
+      "spoiler": pl.Boolean,
+      "stickied": pl.Boolean,
+    }
+
+    return pl.LazyFrame(row_it, schema=schema, strict=False)
+
+
+df = get_comments("data/ADHD_comments.zst")
 
 # %%
-df = df.with_columns(
-    pl.col("selftext")
-    .map_elements(lemmatize, strategy="threading", return_dtype=pl.String)
-    .alias("lemmatized_words")
+df.describe()
+
+# %%
+comments = (
+  df.filter(
+    pl.col("link_id")
+    .map_elements(
+      lambda x: x.removeprefix("t3_"),
+      return_dtype=pl.Utf8,
+      strategy="threading",
+    )
+    .is_in(ids)
+    & pl.col("link_id").eq(pl.col("parent_id"))
+    & pl.col("score").gt(50)
+  )
+  .select("body")
+  .head(10000)
+  .collect()["body"]
+  .to_list()
 )
+len(comments)
 
 
 # %%
-def gen_words(text: str):
-    return gensim.utils.simple_preprocess(text, deacc=True)
+r = random.randint(0, len(comments))
+comments[r]
 
 
 # %%
-df = df.with_columns(
-    pl.col("lemmatized_words")
-    .map_elements(gen_words, strategy="threading", return_dtype=pl.List(str))
-    .alias("gen_words")
-)
+def preprocess(doc: Doc):
+  allowed_postags = ["NOUN", "ADJ", "VERB", "ADV"]
+  items = set()
+  for word in doc:
+    if word.pos_ not in allowed_postags:
+      continue
+    if word.text in stopwords.words():
+      continue
+    items.add(word.lemma_)
+  return items
+
 
 # %%
-id2word = corpora.Dictionary(df["gen_words"])
+def process_with_spacy_pipe(texts, batch_size=2000, n_process=1):
+  nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
+  results = []
+
+  # Process in batches using spaCy's pipe
+  for doc in tqdm(nlp.pipe(texts, batch_size=batch_size, n_process=n_process)):
+    processed = preprocess(doc)
+    results.append(processed)
+
+  return results
+
+
+# %%
+start = time.time()
+res = process_with_spacy_pipe(comments, batch_size=2000, n_process=4)
+print(f"took {time.time() - start} seconds")
+
+
+# %%
+def gen_words(texts):
+  final = []
+  for text in texts:
+    new = gensim.utils.simple_preprocess(" ".join(text), deacc=True)
+    final.append(new)
+  return final
+
+
+words = gen_words(res)
+
+# %%
+bigram_phrases = gensim.models.Phrases(words, min_count=5, threshold=50)
+trigram_phrases = gensim.models.Phrases(bigram_phrases[words], threshold=50)
+
+# %%
+bigrams = gensim.models.phrases.Phrases(bigram_phrases.vocab)
+trigrams = gensim.models.phrases.Phrases(trigram_phrases.vocab)
+
+
+def create_bigrams(texts):
+  return [bigrams[doc] for doc in texts]
+
+
+def create_trigrams(texts):
+  return [trigrams[doc] for doc in texts]
+
+
+data_bigrams = create_bigrams(words)
+data_trigrams = create_trigrams(words)
+
+# %%
+id2word = corpora.Dictionary(data_trigrams)
 
 corpus = []
-for text in df["gen_words"]:
-    new = id2word.doc2bow(text)
-    corpus.append(new)
+# for text in words:
+for text in data_trigrams:
+  new = id2word.doc2bow(text)
+  corpus.append(new)
+
+# %%
+tfidf = gensim.models.TfidfModel(corpus, id2word=id2word)
+
+low_value = 0.1
+high_value = 0.8  # Adjust this value based on your needs
+
+tfwords = []
+tfwords_missing = []
+new_corpus = []
+
+for i in range(len(corpus)):
+  bow = corpus[i]
+  low_value_words = []
+  high_value_words = []
+  for id, value in tfidf[bow]:
+    if value < low_value:
+      low_value_words.append(id)
+    elif value > high_value:
+      high_value_words.append(id)
+    else:
+      tfwords.append((id, value))
+  tfwords_missing.append(low_value_words + high_value_words)
+  new_bow = [
+    b for b in bow if b[0] not in low_value_words and b[0] not in high_value_words
+  ]
+  new_corpus.append(new_bow)
+
 
 # %%
 model = LdaModel(
-    corpus=corpus,
-    id2word=id2word,
-    num_topics=30,
-    random_state=300,
-    update_every=1,
-    chunksize=100,
-    passes=10,
-    alpha="auto",
+  corpus=new_corpus,
+  id2word=id2word,
+  num_topics=30,  # Adjusted number of topics
+  random_state=300,
+  update_every=1,
+  chunksize=100,  # Increased chunksize
+  passes=20,  # Increased number of passes
+  alpha="symmetric",  # Changed alpha
 )
 
 # %%
 vis = pyLDAvis.gensim.prepare(model, corpus, id2word, mds="mmds", R=30)
+
+# %%
+print("keep" in stopwords.words())
--- scraper/util.py
+++ scraper/util.py
@@ -4,103 +4,126 @@ from typing import BinaryIO, Iterator, TypedDict
 import orjson as json
 import zstandard
 
+class Comment(TypedDict):
+  author: str
+  created_utc: str
+  controversiality: int
+  score: int
+  subreddit_id: str
+  link_id: str
+  score_hidden: bool
+  parent_id: str
+  subreddit: str
+  ups: int
+  distinguished: None
+  body: str
+  id: str
+  name: str
+  downs: int
+  archived: bool
+  author_flair_css_class: None
+  retrieved_on: int
+  edited: bool
+  author_flair_text: None
+  gilded: int
+
 
 class Submission(TypedDict):
-    archived: bool
-    author: str
-    author_flair_background_color: str
-    author_flair_css_class: None
-    author_flair_text: None
-    author_flair_text_color: str
-    brand_safe: bool
-    can_gild: bool
-    contest_mode: bool
-    created_utc: int
-    distinguished: None
-    domain: str
-    edited: bool
-    gilded: int
-    hidden: bool
-    hide_score: bool
-    id: str
-    is_crosspostable: bool
-    is_reddit_media_domain: bool
-    is_self: bool
-    is_video: bool
-    link_flair_css_class: None
-    link_flair_richtext: list
-    link_flair_text: None
-    link_flair_text_color: str
-    link_flair_type: str
-    locked: bool
-    media: None
-    media_embed: list
-    no_follow: bool
-    num_comments: int
-    num_crossposts: int
-    over_18: bool
-    parent_whitelist_status: str
-    permalink: str
-    retrieved_on: int
-    rte_mode: str
-    score: int
-    secure_media: None
-    secure_media_embed: list
-    selftext: str
-    send_replies: bool
-    spoiler: bool
-    stickied: bool
-    subreddit: str
-    subreddit_id: str
-    subreddit_name_prefixed: str
-    subreddit_type: str
-    suggested_sort: None
-    thumbnail: str
-    thumbnail_height: None
-    thumbnail_width: None
-    title: str
-    url: str
-    whitelist_status: str
+  archived: bool
+  author: str
+  author_flair_background_color: str
+  author_flair_css_class: None
+  author_flair_text: None
+  author_flair_text_color: str
+  brand_safe: bool
+  can_gild: bool
+  contest_mode: bool
+  created_utc: int
+  distinguished: None
+  domain: str
+  edited: bool
+  gilded: int
+  hidden: bool
+  hide_score: bool
+  id: str
+  is_crosspostable: bool
+  is_reddit_media_domain: bool
+  is_self: bool
+  is_video: bool
+  link_flair_css_class: None
+  link_flair_richtext: list
+  link_flair_text: None
+  link_flair_text_color: str
+  link_flair_type: str
+  locked: bool
+  media: None
+  media_embed: list
+  no_follow: bool
+  num_comments: int
+  num_crossposts: int
+  over_18: bool
+  parent_whitelist_status: str
+  permalink: str
+  retrieved_on: int
+  rte_mode: str
+  score: int
+  secure_media: None
+  secure_media_embed: list
+  selftext: str
+  send_replies: bool
+  spoiler: bool
+  stickied: bool
+  subreddit: str
+  subreddit_id: str
+  subreddit_name_prefixed: str
+  subreddit_type: str
+  suggested_sort: None
+  thumbnail: str
+  thumbnail_height: None
+  thumbnail_width: None
+  title: str
+  url: str
+  whitelist_status: str
 
 
 def get_zst_file_json_stream(
-    f: BinaryIO, chunk_size=1024 * 1024 * 10
-) -> Iterator[Submission]:
-    decompressor = zstandard.ZstdDecompressor(max_window_size=2**31)
-    current_string = ""
+  f: BinaryIO, chunk_size=1024 * 1024 * 10
+) -> Iterator:
+  decompressor = zstandard.ZstdDecompressor(max_window_size=2**31)
+  current_string = ""
 
-    def yieldLinesJson():
-        nonlocal current_string
-        lines = current_string.split("\n")
-        current_string = lines[-1]
-        for line in lines[:-1]:
-            try:
-                yield json.loads(line)
-            except json.JSONDecodeError:
-                print("Error parsing line: " + line)
-                traceback.print_exc()
-                continue
+  def yieldLinesJson():
+    nonlocal current_string
+    lines = current_string.split("\n")
+    current_string = lines[-1]
+    for line in lines[:-1]:
+      try:
+        yield json.loads(line)
+      except json.JSONDecodeError:
+        print("Error parsing line: " + line)
+        traceback.print_exc()
+        continue
 
-    zst_reader = decompressor.stream_reader(f)
-    while True:
-        try:
-            chunk = zst_reader.read(chunk_size)
-        except zstandard.ZstdError:
-            print("Error reading zst chunk")
-            traceback.print_exc()
-            break
-        if not chunk:
-            break
-        current_string += chunk.decode("utf-8", "replace")
-
-        yield from yieldLinesJson()
+  zst_reader = decompressor.stream_reader(f)
+  while True:
+    try:
+      chunk = zst_reader.read(chunk_size)
+    except zstandard.ZstdError:
+      print("Error reading zst chunk")
+      traceback.print_exc()
+      break
+    if not chunk:
+      break
+    current_string += chunk.decode("utf-8", "replace")
 
     yield from yieldLinesJson()
 
-    if len(current_string) > 0:
-        try:
-            yield json.loads(current_string)
-        except json.JSONDecodeError:
-            print("Error parsing line: " + current_string)
-            print(traceback.format_exc())
-            pass
+  yield from yieldLinesJson()
+
+  if len(current_string) > 0:
+    try:
+      yield json.loads(current_string)
+    except json.JSONDecodeError:
+      print("Error parsing line: " + current_string)
+      print(traceback.format_exc())
+      pass
